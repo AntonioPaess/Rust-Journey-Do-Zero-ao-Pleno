ef02fdbd31839d6ae91adad3fef5bcdefbd177ef
Antônio Paes <paesdeandradeantonio@gmail.com>
2025-01-26 23:38:03 -0300

    docs: Documenta implementação da função para encontrar o maior número em vetor
    Adiciona implementação e explicação de uma função em Rust que recebe um vetor de inteiros como parâmetro e retorna o maior número.
    Inclui boas práticas, como inicialização robusta da variável maior e desreferenciamento automático durante a iteração.
    Essa implementação faz parte do curso que está sendo documentado no repositório, destacando conceitos aprendidos e aplicados.

--- Modulo_1/desafios_1..=12/Tarefa_2.rs
+++ Modulo_1/desafios_1..=12/Tarefa_2.rs
@@ -2,8 +2,30 @@
  que receba um vetor de números inteiros e retorne o maior número encontrado nele. 
 Essa função deve ser capaz de percorrer o vetor e encontrar o maior valor dentro dele. */
 
+fn maior(lista: &[i32]) -> i32{
+    let mut maior:i32 = lista[0];
+
+    if lista.is_empty(){
+        return 1;
+    }
+    
+
+    for &i in lista{
+        if i > maior{
+            maior = i;
+        }
+    }
+    return maior;
+}
 
 
 fn main(){
+    let lista = vec! {1, 2, -2000, 80, 40, 34};
 
-}
+    if maior(&lista) == 1{
+        println!("O vetor está vazio");
+    }
+    else{
+        println!("O maior número é: {}", maior(&lista));
+    }
+} 
